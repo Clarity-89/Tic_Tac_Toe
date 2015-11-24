0f192dd10cbef2b61cef6b37e1d895d0437c3df1
Clarity-89 <homes89@ukr.net>
2015-11-24 22:56:38 +0200

    Working minimax

--- app.js
+++ app.js
@@ -91,19 +91,19 @@ $(document).ready(function () {
         //check rows
         for (var i = 0; i < this.dims; i++) {
 
-            if (this.square([i],[0]) !== 0 && this.square([i],[0]) == this.square([i],[1]) && this.square([i],[0]) == this.square([i],[2])) {
+            if (this.square([i], [0]) !== 0 && this.square([i], [0]) == this.square([i], [1]) && this.square([i], [0]) == this.square([i], [2])) {
                 //console.log('first')
-                return this.square([i],[0]);
+                return this.square([i], [0]);
                 //check cols
-            } else if (this.square([0],[i]) !== 0 && this.square([0],[i]) == this.square([1],[i]) && this.square([0],[i]) == this.square([2],[i])) {
+            } else if (this.square([0], [i]) !== 0 && this.square([0], [i]) == this.square([1], [i]) && this.square([0], [i]) == this.square([2], [i])) {
                 //console.log('second', i)
-                return this.square([0],[i]);
+                return this.square([0], [i]);
             }
         }
         //check diags
-        if (this.square([1],[1]) !== 0 && (this.square([0],[0]) == this.square([1],[1]) && this.square([0],[0]) == this.square([2],[2]) ||
-            this.square([0],[2]) == this.square([1],[1]) && this.square([0],[2]) == this.square([2],[0]))) {
-            return this.square([1],[1]);
+        if (this.square([1], [1]) !== 0 && (this.square([0], [0]) == this.square([1], [1]) && this.square([0], [0]) == this.square([2], [2]) ||
+            this.square([0], [2]) == this.square([1], [1]) && this.square([0], [2]) == this.square([2], [0]))) {
+            return this.square([1], [1]);
             //check draw
         } else if (this.getEmptySquares().length === 0) {
             return DRAW;
@@ -119,87 +119,31 @@ $(document).ready(function () {
         //return new Board(3, this.grid.concat());
     };
 
-
-    /* function minimax(board, player) {
-     if (board.checkWin() != 'None') {
-     return [SCORES[board.checkWin()], [-1, -1]];
-     } else {
-     var best;
-     if (player == PLAYERX) {
-     best = [-2, ''];
-     board.getEmptySquares().forEach(function (square) {
-     //console.log('sq', square);
-     var copy = board.clone();
-     copy.move(square, player);
-     var score = minimax(copy, switchPlayer(player))[0];
-     if (score == 1) {
-     return [score, square];
-     }
-     if (score > best[0]) {
-     best = [score, square];
-     }
-     });
-     return best;
-     } else {
-     best = [2, ''];
-     board.getEmptySquares().forEach(function (square) {
-     //console.log('sq', square);
-     var copy = board.clone();
-     copy.move(square, player);
-     var score = minimax(copy, switchPlayer(player))[0];
-     if (score == -1) {
-     return [score, square];
-     }
-     if (score < best[0]) {
-     best = [score, square];
-     }
-     });
-     return best;
-     }
-     }
-     }*/
-
     function minimax(board, player) {
         //console.log(board, player)
         var mult = SCORES['' + player], thisScore;
         var maxScore = -1, bestMove = null;
 
-        //console.log('mult', mult)
-        //if (player == PLAYERO) mult = -1;
         if (board.checkWin() != 'None') {
-            //console.log('cw', SCORES[board.checkWin()])
             return [SCORES[board.checkWin()], [-1, -1]];
+        } else {
+            board.getEmptySquares().forEach(function (square) {
+
+                var copy = board.clone();
+                //console.log('Board', board, 'copy', copy)
+                copy.move(square, player);
+                thisScore = mult * minimax(copy, switchPlayer(player))[0];
+                //console.log(square, player, thisScore)
+                if (thisScore >= maxScore) {
+                    maxScore = thisScore;
+                    bestMove = square;
+                }
+            });
+
+            return [mult * maxScore, bestMove];
         }
-        board.getEmptySquares().forEach(function (square) {
-
-            var copy = board.clone();
-            //console.log('Board', board, 'copy', copy)
-            copy.move(square, player);
-            thisScore = mult * minimax(copy, switchPlayer(player))[0];
-            //console.log(square, player, thisScore)
-            if (thisScore >= maxScore) {
-                maxScore = thisScore;
-                bestMove = square;
-            }
-        });
-
-        return [mult * maxScore, bestMove];
-
     }
 
-    /*function mmMove(board, player){
-     var thisScore, bestScore = -1, bestMove = null;
-     board.getEmptySquares().forEach(function (square) {
-     var copy = board.clone();
-     copy.move(square, player);
-     thisScore = minimax(copy, switchPlayer(player));
-     if(thisScore >= bestScore) {
-     bestScore = thisScore;
-     bestMove = square;
-     }
-     });
-     return bestMove;
-     }*/
 
     //Function that runs the game
     function runGame(dims) {
@@ -222,12 +166,9 @@ $(document).ready(function () {
                 tripleT.move(coords[id], PLAYERX);
 
                 if (tripleT.checkWin() === 'None') {
-                    //tripleT.showGrid();
-                    var move = minimax(tripleT, PLAYERX)[1];
-                    //console.log('move', move)
+                    var move = minimax(tripleT, PLAYERO)[1];
                     tripleT.move(move, PLAYERO);
                     id = subArrayIndex(coords, move);
-                    //console.log('id', id)
                     $('#' + id).text('O');
                 } else {
                     alert('winner is ' + tripleT.checkWin())
@@ -279,17 +220,17 @@ $(document).ready(function () {
 
     runGame(dims);
 
-    var test = new Board(dims, [[2, 0, 0], [1, 2, 0], [1, 1, 0]]);
-    var test2 = new Board(dims, test.grid.concat());
-    //var test2 = test.clone();
-    console.log('test grid', test.showGrid());
-    console.log('test2 grid', test2.showGrid());
-    test.move([0, 1], PLAYERO);
-    console.log('test grid', test.showGrid());
-    console.log('test2 grid', test2.showGrid());
-    var ttt = new Board(3, test2);
-    //console.log('min', minimax(ttt, PLAYERO)[1]);
-    /*ttt.move(2, 1, PLAYERX);
+    /*var test = new Board(dims, [[2, 0, 0], [1, 2, 0], [1, 1, 0]]);
+     var test2 = new Board(dims, test.grid.concat());
+     //var test2 = test.clone();
+     console.log('test grid', test.square(0, 1));
+     console.log('test2 grid', test2.showGrid());
+     test.move([0, 1], PLAYERO);
+     console.log('test grid', test.square(0, 1));
+     console.log('test2 grid', test2.showGrid());
+     var ttt = new Board(3, test2);
+     //console.log('min', minimax(ttt, PLAYERO)[1]);
+     /*ttt.move(2, 1, PLAYERX);
      ttt.showGrid();
      console.log('winner', ttt.checkWin());*/
 
