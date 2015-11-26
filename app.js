4d5fed3df9974f49c5c1136c2df96ad6427e82b9
Clarity-89 <homes89@ukr.net>
2015-11-26 14:05:33 +0200

    Add mobile-friendly css

--- app.js
+++ app.js
@@ -156,7 +156,6 @@ $(document).ready(function () {
                 if (board.checkWin() === 'None') {
                     AImove(board);
                 } else {
-                    sq = undefined;
                     declareWinner(board.checkWin());
                 }
             } else {
