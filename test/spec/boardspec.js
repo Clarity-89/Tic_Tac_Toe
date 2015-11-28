f3718abb68779f3e317634f3ad124eeedc7982d0
Clarity-89 <homes89@ukr.net>
2015-11-28 18:06:56 +0200

    Refactor tests

--- test/spec/boardspec.js
+++ test/spec/boardspec.js
@@ -17,13 +17,10 @@ describe('Get empty squares', function () {
     it('should show all empty squares available for current grid', function () {
         board = new Board(3, [0, 1, 2, 1, 2, 1, 1, 2, 1]);
         expect(board.getEmptySquares().length).toBe(1);
-    });
 
-    it('should show all empty squares available for current grid', function () {
         board = new Board(3, [1, 1, 0, 1, 2, 1, 1, 0, 1]);
         expect(board.getEmptySquares().length).toBe(2);
-    });
-    it('should show all empty squares available for current grid', function () {
+
         board = new Board(3);
         expect(board.getEmptySquares().length).toBe(9);
     });
@@ -34,195 +31,165 @@ describe('Get empty squares', function () {
     });
 });
 
-describe('Check Win, player 1', function () {
+describe('Check Win', function () {
     it('should show when player 1 wins correctly', function () {
         board = new Board(3,
             [1, 1, 1,
-             2, 2, 1,
-             2, 1, 2]);
+                2, 2, 1,
+                2, 1, 2]);
         expect(board.checkWin()).toBe(1);
-    });
 
-    it('should show when player 1 wins correctly', function () {
         board = new Board(3,
             [0, 2, 2,
-             1, 1, 1,
-             0, 2, 2]);
+                1, 1, 1,
+                0, 2, 2]);
         expect(board.checkWin()).toBe(1);
-    });
 
-    it('should show when player 1 wins correctly', function () {
         board = new Board(3,
             [1, 2, 2,
-             2, 1, 2,
-             1, 1, 1]);
+                2, 1, 2,
+                1, 1, 1]);
         expect(board.checkWin()).toBe(1);
-    });
 
-    it('should show when player 1 wins correctly', function () {
         board = new Board(3,
             [1, 2, 2,
-             1, 2, 2,
-             1, 1, 0]);
+                1, 2, 2,
+                1, 1, 0]);
         expect(board.checkWin()).toBe(1);
-    });
 
-    it('should show when player 1 wins correctly', function () {
         board = new Board(3,
             [2, 1, 2,
-             0, 1, 2,
-             0, 1, 0]);
+                0, 1, 2,
+                0, 1, 0]);
+        expect(board.checkWin()).toBe(1);
+
+        board = new Board(3,
+            [2, 2, 1,
+                0, 2, 1,
+                0, 0, 1]);
+        expect(board.checkWin()).toBe(1);
+
+        board = new Board(3,
+            [1, 2, 1,
+                2, 1, 2,
+                1, 0, 1]);
+        expect(board.checkWin()).toBe(1);
+
+        board = new Board(3,
+            [2, 2, 1,
+                0, 1, 2,
+                1, 0, 0]);
         expect(board.checkWin()).toBe(1);
     });
 
-    it('should show when player 1 wins correctly', function () {
-        board = new Board(3,
-            [2, 2, 1,
-             0, 2, 1,
-             0, 0, 1]);
-        expect(board.checkWin()).toBe(1);
-    });
-    it('should show when player 1 wins correctly', function () {
-        board = new Board(3,
-            [1, 2, 1,
-             2, 1, 2,
-             1, 0, 1]);
-        expect(board.checkWin()).toBe(1);
-    });
-    it('should show when player 1 wins correctly', function () {
-        board = new Board(3,
-            [2, 2, 1,
-             0, 1, 2,
-             1, 0, 0]);
-        expect(board.checkWin()).toBe(1);
-    });
-});
-describe('Check win, player 2', function () {
     it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [2, 2, 2,
-             0, 1, 1,
-             0, 1, 1]);
+                0, 1, 1,
+                0, 1, 1]);
         expect(board.checkWin()).toBe(2);
-    });
 
-    it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [0, 1, 1,
-             2, 2, 2,
-             0, 1, 1]);
+                2, 2, 2,
+                0, 1, 1]);
         expect(board.checkWin()).toBe(2);
-    });
 
-    it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [0, 1, 1,
-             0, 1, 1,
-             2, 2, 2]);
+                0, 1, 1,
+                2, 2, 2]);
         expect(board.checkWin()).toBe(2);
-    });
 
-    it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [2, 1, 0,
-             2, 0, 1,
-             2, 1, 0]);
+                2, 0, 1,
+                2, 1, 0]);
         expect(board.checkWin()).toBe(2);
-    });
 
-    it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [1, 2, 1,
-             0, 2, 1,
-             0, 2, 0]);
+                0, 2, 1,
+                0, 2, 0]);
         expect(board.checkWin()).toBe(2);
-    });
 
-    it('should show when player 2 wins correctly', function () {
         board = new Board(3,
             [1, 1, 2,
-             1, 1, 2,
-             0, 0, 2]);
+                1, 1, 2,
+                0, 0, 2]);
         expect(board.checkWin()).toBe(2);
-    });
-    it('should show when player 2 wins correctly', function () {
+
         board = new Board(3,
             [2, 1, 1,
-             2, 2, 1,
-             1, 0, 2]);
+                2, 2, 1,
+                1, 0, 2]);
         expect(board.checkWin()).toBe(2);
-    });
-    it('should show when player 2 wins correctly', function () {
-        board = new Board(3,
-            [1, 1, 2,
-             0, 2, 1,
-             2, 0, 0]);
-        expect(board.checkWin()).toBe(2);
-    });
-});
 
-describe('Check win, Draw', function () {
-    it('should correctly identify when game draws', function () {
         board = new Board(3,
             [1, 1, 2,
-             2, 2, 1,
-             1, 2, 1]);
-        expect(board.checkWin()).toBe(DRAW);
+                0, 2, 1,
+                2, 0, 0]);
+        expect(board.checkWin()).toBe(2);
     });
-    it('should correctly identify when game draws', function () {
+
+    it('should correctly identify when the game draws', function () {
+        board = new Board(3,
+            [1, 1, 2,
+                2, 2, 1,
+                1, 2, 1]);
+        expect(board.checkWin()).toBe(DRAW);
+
         board = new Board(3,
             [1, 2, 2,
-             2, 1, 1,
-             1, 2, 2]);
+                2, 1, 1,
+                1, 2, 2]);
         expect(board.checkWin()).toBe(DRAW);
+
     });
+
     it('should not show draw when one of the players wins', function () {
         board = new Board(3,
             [1, 2, 1,
-             2, 1, 1,
-             1, 2, 2]);
+                2, 1, 1,
+                1, 2, 2]);
         expect(board.checkWin()).not.toBe(DRAW);
     });
     it('should not show draw when game is still in process', function () {
         board = new Board(3,
             [1, 2, 1,
-             2, 1, 1,
-             1, 0, 2]);
+                2, 1, 1,
+                1, 0, 2]);
         expect(board.checkWin()).not.toBe(DRAW);
     });
-});
 
-describe('Check win, None', function () {
-    it('should correctly identify when the game is still in progress, #1', function () {
+    it('should correctly identify when the game is still in progress', function () {
         board = new Board(3,
             [1, 0, 1,
-             2, 0, 1,
-             1, 0, 2]);
+                2, 0, 1,
+                1, 0, 2]);
         expect(board.checkWin()).toBe('None');
-    });
 
-    it('should correctly identify when the game is still in progress, #2', function () {
         board = new Board(3,
             [1, 2, 1,
-             2, 1, 1,
-             0, 1, 2]);
+                2, 1, 1,
+                0, 1, 2]);
         expect(board.checkWin()).toBe('None');
     });
 });
 
 describe('Clone method', function () {
-    it('should make a copy of a passed board', function(){
-        board = new Board(3, [1,0,0,0,0,0,0,0,0]);
+    it('should make a copy of a passed board', function () {
+        board = new Board(3, [1, 0, 0, 0, 0, 0, 0, 0, 0]);
         var copy = board.clone();
 
         expect(board).toEqual(copy);
     });
 
-    it('the original board should not be affected by changes to copy', function(){
-        board = new Board(3, [1,0,0,0,0,0,0,0,0]);
+    it('the original board should not be affected by changes to copy', function () {
+        board = new Board(3, [1, 0, 0, 0, 0, 0, 0, 0, 0]);
         var copy = board.clone();
         copy.move(2, PLAYERO);
         expect(board.grid).not.toEqual(copy.grid);
-        expect(board.grid).toEqual([1,0,0,0,0,0,0,0,0]);
+        expect(board.grid).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 0]);
     });
 });
